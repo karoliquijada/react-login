0c969aff3855e237e460c30e698175674248adab
karoliquijada <kquijada9894@gmail.com>
2022-01-04 00:22:47 -0400

    agregue todos los elementos a la card de inicio

--- src/components/RegisterCard/RegisterCard.js
+++ src/components/RegisterCard/RegisterCard.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Button, TextField, Card } from '@mui/material';
+import { Button, TextField, Card, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
 
 
-const RegisterCard = ({id, name, username, content }) => {
+const RegisterCard = ({ username, password }) => {
   return (
     <Card className="card-style" variant="outlined">
          <h1>Sign in</h1>
          <form>
             <TextField id="username-tf" label="Username" variant="standard" />
             <TextField id="password-tf" label="Password" variant="standard" />
+            <FormGroup>
+              <FormControlLabel control={<Checkbox defaultChecked />} label="Keep me sign in"/>
+            </FormGroup>
             <Button variant="contained" color="secondary">Sign in</Button>
+            <p>Not registered? Sign up now!</p>
+            
         </form>
     </Card>
   );
