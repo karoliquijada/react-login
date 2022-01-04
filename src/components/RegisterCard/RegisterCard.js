60139c307f64363157980a082649c58458e4033f
karoliquijada <kquijada9894@gmail.com>
2022-01-04 12:37:39 -0400

    formik para validar el formulario

--- src/components/RegisterCard/RegisterCard.js
+++ src/components/RegisterCard/RegisterCard.js
@@ -1,20 +1,59 @@
 import React from 'react';
 import { Button, TextField, Card, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 
+const validationSchema = yup.object({
+  username: yup
+    .string('Enter your username')
+    .required('Username is required'),
+  password: yup
+    .string('Enter your password')
+    .min(8, 'Password should be of minimum 8 characters length')
+    .required('Password is required'),
+});
 
 const RegisterCard = ({ username, password }) => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <Card className="card-style" variant="outlined">
          <h1>Sign in</h1>
-         <form>
-            <TextField id="username-tf" label="Username" variant="standard" />
-            <TextField id="password-tf" label="Password" variant="standard" />
+         <form onSubmit={formik.handleSubmit}>
+            <TextField 
+              id="username-tf" 
+              name="username" 
+              label="Username" 
+              variant="standard"
+              onChange={formik.handleChange}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username} 
+            />
+            <TextField 
+              id="password-tf" 
+              name="password"
+              label="Password" 
+              type="password"
+              variant="standard"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
             <FormGroup>
               <FormControlLabel control={<Checkbox defaultChecked />} label="Keep me sign in"/>
             </FormGroup>
-            <Button variant="contained" color="secondary">Sign in</Button>
-            <p>Not registered? Sign up now!</p>
-            
+            <Button variant="contained" color="secondary" type="submit">Sign in</Button>
+            <p>Not registered? Sign up now!</p>            
         </form>
     </Card>
   );
@@ -22,4 +61,5 @@ const RegisterCard = ({ username, password }) => {
 }
 
 export default RegisterCard;
+
 
